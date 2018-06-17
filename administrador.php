21e1210cadef0ad9b257c6cb61f3692ca1cae576
gobenavides <gobenavides@udec.cl>
2018-06-17 00:13:20 -0400

    Algunos avances en administrador.php

--- administrador.php
+++ administrador.php
@@ -1,9 +1,3 @@
-﻿<?php
-include("connect_db.php");
-?>
-
-
-
 <html lang="es">
 <head>
   <meta charset="UTF-8">
@@ -13,10 +7,77 @@ include("connect_db.php");
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 
-<body>
-  <h2> Sección administrador </h2>
-  <p><form action='admin.php' method=post>
-    <input type="submit"value="Administrador">
-  </p>
-</body>
-</html>
+<nav class="navbar navbar-default"> <!-- Todo lo que estè dentro de nav sera la barra de navegacion -->
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Sección Administrador </a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav"> <!-- esto es una lista desordenada -->
+
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Registrar <span class="caret"></span></a>
+          <ul class="dropdown-menu"> <!-- es una lista tipo drop dawn, la clase="dropdown-menu" es un estilo que tiene bootstrap el cual añadimos en el head -->
+            <li><a href="elegir_ayudantes.php">Elegir alumnos ayudantes</a></li>
+            <li><a href="pasar_semestre.php">Limpiar base para pasar a semestre siguiente</a></li>
+            <li role="separator" class="divider"></li> <!-- separa visualmente la lista  -->
+            <li><a href="Ingredientes.html">Algo que aún no se me ocurre jaja</a></li>
+
+
+          </ul>
+
+        </li>
+         <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Consultar <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="postulantes_del_semestre.php">Postulantes del semestre actual</a></li>
+
+
+          </ul>
+
+        </li>
+      </ul>
+
+
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+<?php
+include("connect_db.php");
+$sql="SELECT * FROM postulante";
+$result = $mysqli->query($sql);
+?>
+
+<div class="container">
+  <h2>Tabla</h2>
+  <p>Tabla de ejemplo</p>
+  <div class="table-responsive">
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Matrícula</th>
+          <th>Nombre</th>
+          <th>Correo</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <?php while ($row = mysqli_fetch_array($result)) { ?>
+          <tr>
+            <td><?php echo $row[0]; ?></td>
+            <td><?php echo $row[1]; ?></td>
+            <td><?php echo $row[2]; ?></td>
+          </tr>
+        <?php } ?>
+      </tbody>
+    </table>
+  </div>
+</div>
