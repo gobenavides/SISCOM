bc272f9a08c0a7e228a51c50cf54da2838742526
gobenavides <gobenavides@udec.cl>
2018-06-21 00:26:53 -0400

    Agregado tabla con los postulantes a los que le calza el horario.
    Hay que arreglar detalles, no está funcionando.

--- postulantes_de_ramo.php
+++ postulantes_de_ramo.php
@@ -63,7 +63,18 @@ include("connect_db.php");
 $cod_ramo= $_POST["codigo_ramo"];
 $sql="SELECT postulante.nombre,postulante.matricula,postulante.correo
 FROM postulante, postula WHERE postulante.matricula=postula.matricula AND postula.codigo='".$cod_ramo."'";
+
+$sql2="SELECT postulante.nombre,postulante.matricula,postulante.correo FROM postulante, postula,dispone,tiene
+WHERE postulante.matricula=postula.matricula AND postula.codigo='$cod_ramo' AND dispone.matricula=postulante.matricula AND tiene.codigo='$cod_ramo'
+AND postulante.matricula = ALL (SELECT dispone.matricula FROM dispone,tiene WHERE dispone.dia=tiene.dia AND dispone.hora=tiene.hora )";
+
+/* FALTA ARREGLAR LA ÚLTIMA CONSULTA */
+
+
+echo $sql2;
+
 $tabla = $mysqli->query($sql);
+$tabla2= $mysqli->query($sql2);
 ?>
 
 <body>
@@ -82,6 +93,33 @@ $tabla = $mysqli->query($sql);
 
       <tbody>
         <?php while ($row = mysqli_fetch_array($tabla)) { ?>
+          <tr>
+            <td><?php echo $row[0]; ?></td>
+            <td><?php echo $row[1]; ?></td>
+            <td><?php echo $row[2]; ?></td>
+            <td><a href="info_postulante.php?post_matricula=<?php echo $row[1]; ?>">(ver detalles)</a></td>
+
+          </tr>
+        <?php } ?>
+      </tbody>
+    </table>
+  </div>
+
+  <div class="container">
+    <h2>Postulantes de <?php echo $cod_ramo;?> con disponibilidad de horario compatible</h2>
+    <p>Tabla que muestra todos los postulantes del ramo <?php echo $cod_ramo;?> seleccionado del semestre actual con disponibilidad compatible.</p>
+    <table class="table table-dark">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col">Nombre</th>
+          <th scope="col">Matrícula</th>
+          <th scope="col">Correo</th>
+          <th scope="col"> </th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <?php while ($row = mysqli_fetch_array($tabla2)) { ?>
           <tr>
             <td><?php echo $row[0]; ?></td>
             <td><?php echo $row[1]; ?></td>
