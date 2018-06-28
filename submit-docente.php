15fa02346a6757fb649028d8fda0759d644acefa
IsabelCamila <isa.donoso25@gmail.com>
2018-06-28 18:55:59 -0400

    punto y coma de nuevo

--- submit-docente.php
+++ submit-docente.php
@@ -6,7 +6,7 @@ if (strlen($rut)==12 or strlen($rut)==11){
 $query="INSERT INTO profesor(rut, nombre) VALUES ('$rut','$nombre')";
 $resultado= $mysqli->query($query);
 
-header("Location:add_nuevos.php")
+header("Location:add_nuevos.php");
 }else{
   echo "el rut ingresado no es válido";
 }
